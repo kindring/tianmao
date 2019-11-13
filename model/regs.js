a11ea4c27cf4b005b70ac3c74b77c4990c720081
kindring <43570639+kindring@users.noreply.github.com>
2019-11-13 22:01:28 +0800

    正则验证模块

---
+++ model/regs.js
@@ -0,0 +1,70 @@
+let regs = [
+    {
+        reg:/^\d{9,16}$/,
+        fields:['account']
+    },
+    {
+        reg:/^\w{8,16}$/,
+        fields:['password']
+    },
+    {
+        reg:/^\d+$/,
+        fields:['id','staff_id','func_id','authority_level','level','department_id']
+    },
+    {
+        reg:/^[\u4E00-\u9FA5A-Za-z0-9_]+$/,
+        fields:['name','staff_name']
+    },
+    {
+        reg:/^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/,
+        fields:['phone','staff_account']
+    }
+];
+function add_field(reg,field){
+    for(var i = 0; i < regs.length;i++){
+        if(regs[i].reg==reg){
+            regs[i].fields.push(field);
+        }else{
+            console.log(regs[i].reg==reg)
+        }
+    }
+}
+function getreg(field){
+    var result=null;
+    for(var i in regs){
+        for(var j in regs[i].fields){
+            if(regs[i].fields[j]==field){
+               result=regs[i].reg;
+               break;
+            }
+        }
+    }
+    if(!result){
+        result=/^[\u4E00-\u9FA5A-Za-z0-9_]+$/
+    }
+    return result
+}
+function test(field,value){
+    console.log(field,value);
+    console.log(getreg(field));
+    console.log(getreg(field).test(value));
+    return getreg(field).test(value)
+}
+console.log(getreg('password').test('abcderEf1222457'));
+try{
+    if(window){
+        window.$reg={
+            test:test
+        }
+    }
+}catch(e){
+}
+try{
+    if(module){
+        module.exports.getreg=getreg;//根据传入的字段返回正则
+        module.exports.test=test;
+    }
+}catch (e) {
+
+}
+
