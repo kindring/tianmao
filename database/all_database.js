f25bb23c9bad2c23c27709d9661e54f0ee76671e
kindring <43570639+kindring@users.noreply.github.com>
2019-12-16 20:53:39 +0800

    导出数据库

--- database/all_database.js
+++ database/all_database.js
@@ -1,5 +1,11 @@
 //只用来统一导出数据库封装的方法,不做查询等操作
 const platform = require('./platform.js');
 const platform_goods = require('./platform_goods.js');
+const address = require('./address.js');
+const merchant = require('./merchant.js');
+const goods = require('./goods.js');
 module.exports.platform=platform;
 module.exports.platform_goods=platform_goods;
+module.exports.address=address;
+module.exports.merchant=merchant;
+module.exports.goods=goods;
