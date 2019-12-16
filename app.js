35f65d027e37e332894497a1350691f3be547fac
kindring <43570639+kindring@users.noreply.github.com>
2019-12-16 20:43:51 +0800

    商家模块

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const session = require('express-session');
 const sockets = require('./model/sockets.js');//socktes列表,用来作为全局作用域存储所有的socket连接
 
 const server_config = require('./server_config.json');
-const platform = require('./routers/platform');
-const platform_goods = require('./routers/platform_goods');
-
+const platform = require('./routers/platform');//平台管理路由
+const platform_goods = require('./routers/platform_goods');//平台与商品之间的路由
+const public_interface = require('./routers/public_interface');//公共接口
+const merchant = require('./routers/merchant');//商家路由
+const goods = require('./routers/goods.js');//商品路由
+let public_path = require('./model/public_file_path.js');
 let app = express();
 app.use(session({
         secret: 'tmshop',
@@ -33,15 +36,16 @@ app.use('/css',express.static(path.join(__dirname,server_config.static_path,'/cs
 app.use('/model',express.static(path.join(__dirname,'/model')));
 app.use('/picture',express.static(path.join(__dirname,'/picture')));
 // app.use('/vue',express.static(path.join(__dirname,'/vue')));
-app.use(bodyParser.urlencoded());
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true }));
+app.use(bodyParser.json({limit: '50mb'}));
 // console.log(platform);
-
 app.use('/platform',platform);
 app.use('/platform_goods',platform_goods);
-
+app.use('/merchant',merchant);
+app.use('/goods',goods);
+app.use(public_interface);//公共接口
 app.use('*',function(req,res){
-    res.send("404 page not found"+req.url)
+    res.send("404 page not found"+req.path)
 });
 var server = app.listen(80,()=>{
     console.log("server is running");
@@ -62,3 +66,6 @@ io.on('connection',function(socket){
         }
     });
 });
+
+//使用全局变量将部分数据存储在其模块中
+public_path.paths['index']=__dirname;
