3d12ccde36496d1924c6308b47afe708ce729aa7
kindring <43570639+kindring@users.noreply.github.com>
2019-12-30 17:05:46 +0800

    将其改为使用配置文件来进行连接数据库,方便合作使用

--- database/pool.js
+++ database/pool.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql');
+const databseConfig = require('../config/database.json');//数据库连接的配置文件
 //作用
 //导出全局连接池对象,使其可以统一使用此连接池操作数据库
 const pool = mysql.createPool({
-    connectionLimit:100,
-    host:'127.0.0.1',
-    user:'root',
-    password:'password',
-    database:'tm_test'
+    connectionLimit:databseConfig.connectionLimit||100,//连接限制
+    host:databseConfig.host,//地址
+    user:databseConfig.user,//用户
+    password:databseConfig.password,//密码
+    database:databseConfig.database//数据库名称
 });//创建连接池对象
 module.exports=pool;
 
